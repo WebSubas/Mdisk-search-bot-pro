85a69646c5148394e61640d611ee4994f20520c1
WebSubas <122187148+WebSubas@users.noreply.github.com>
2023-01-08 06:43:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/WebSubas/Mdisk-Search-pro-bot.git /Mdisk-Search-pro-Bot
+cd /Mdisk-Search-pro-Bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
